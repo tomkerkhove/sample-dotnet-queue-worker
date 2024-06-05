b04e2c1ca3e71aa3fee058cfe4e1bf9133a73ed2
Richard87 <richard.hagen@gmail.com>
2024-06-05 17:19:21 +0200

    cleanup azure functions test
    Signed-off-by: Richard87 <richard.hagen@gmail.com>

--- src/Radix.Samples.Dotnet.OrderProcessor/Program.cs
+++ src/Radix.Samples.Dotnet.OrderProcessor/Program.cs
@@ -2,20 +2,7 @@ using Radix.Samples.Dotnet.Contracts;
 using Radix.Samples.Dotnet.OrderProcessor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Hosting;
-//
-// var builder = new HostBuilder()
-//         .ConfigureServices((context, services) =>
-//         {
-//             services.AddOrderQueueServices();
-//             services.AddAzureClients(clientBuilder =>
-//             {
-//                 clientBuilder.AddServiceBusClient();
-//             });
-//         })
-//
-//     ;
 
 var builder = Host.CreateApplicationBuilder();
 builder.Configuration.AddJsonFile("appsettings.local.json", optional: true);
